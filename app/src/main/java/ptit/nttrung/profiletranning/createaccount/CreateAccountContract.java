e15c9735491ffd3add241d3dbb9abf063d577476
trung <thanhtrungnguyen1208@gmail.com>
2017-09-02 22:39:10 +0700

    Day 2

--- app/src/main/java/ptit/nttrung/profiletranning/createaccount/CreateAccountContract.java
+++ app/src/main/java/ptit/nttrung/profiletranning/createaccount/CreateAccountContract.java
@@ -1,8 +1,42 @@
 package ptit.nttrung.profiletranning.createaccount;
 
+import android.support.annotation.StringRes;
+
+import ptit.nttrung.profiletranning.BasePresenter;
+import ptit.nttrung.profiletranning.BaseView;
+
 /**
  * Created by TrungNguyen on 8/28/2017.
  */
 
 public interface CreateAccountContract {
+    interface View extends BaseView<Presenter> {
+        void makeToast(@StringRes int stringId);
+
+        String getEmail();
+
+        String getPassword();
+
+        String getPasswordConfirmation();
+
+        String getName();
+
+        //TODO: add name input to this component
+
+        void startLoginActivity();
+
+        void startProfilePageActivity();
+
+        void setPresenter(CreateAccountContract.Presenter presenter);
+
+        void showProgressIndicator(boolean show);
+    }
+
+    interface Presenter extends BasePresenter {
+        void onCreateAccountClick();
+
+        void subscribe();
+
+        void unsubscribe();
+    }
 }
